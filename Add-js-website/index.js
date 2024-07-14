08698bd07da129721251c2cb66c1f4ce95320c2f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-13 22:47:26 -0300

    Iniciando .classList.add

--- Add-js-website/index.js
+++ Add-js-website/index.js
@@ -1,13 +1,17 @@
 // Selecionando elemntos pela TagName
-document.getElementsByTagName("li")[0].style.color = "red"  
+document.getElementsByTagName("li")[0].style.color = "red"
+
 
 
 // Selecionando elemntos pela ClassName
 document.getElementsByClassName("btn")[0].innerHTML = "click "
-
+document.getElementsByClassName("btn")[0].style.backgroundColor =  "aqua"
 // Selecionando com qualquer tag
 document.querySelector("p.randonTag").style.color = "blue"
 
 
 // Selecionando varios elementos
-ducument.querySelectorAll("#list .item")[1].style.color = "red"
+// ducument.querySelectorAll("#list .item")[1].style.color = "red"
+
+
+document.querySelector("paragrafoInicio")[0].classList.add("P1")
