d354822642799b7370f830086ecabab4d2210564
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-13 22:13:58 -0300

    Compreendendo o ducument.getElemnts e ducument.querySelector

--- Add-js-website/index.js
+++ Add-js-website/index.js
@@ -1,1 +1,13 @@
-document.querySelector("h1").innerHTML = "Good bye"
+// Selecionando elemntos pela TagName
+document.getElementsByTagName("li")[0].style.color = "red"  
+
+
+// Selecionando elemntos pela ClassName
+document.getElementsByClassName("btn")[0].innerHTML = "click "
+
+// Selecionando com qualquer tag
+document.querySelector("p.randonTag").style.color = "blue"
+
+
+// Selecionando varios elementos
+ducument.querySelectorAll("#list .item")[1].style.color = "red"
